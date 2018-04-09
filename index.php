bcfd33570bf9b481459560818e47762b68204b2a
marquezquez <38035270+marquezquez@users.noreply.github.com>
2018-04-09 10:20:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,129 +1,109 @@
 
-<HEAD>
-   <TITLE>Inserción de Equipos de Futbol. </TITLE>
-   <LINK REL="stylesheet" TYPE="text/css" >
-</HEAD>
+<html>
 
-<BODY> 
-  
-<?PHP
-   
-   if (isset($_REQUEST['insertar']))
-   {
-   // Obtener valores introducidos en el formulario
-      $nomequipo = $_REQUEST['nequipo'];
-	  $nomciudad = $_REQUEST['nciudad'];
-      $nompresidente = $_REQUEST['npresidente'];
-      $aniofundacion = $_REQUEST['naniofundacion'];
-	  $numgoles2015 = $_REQUEST['ngoles2015'];
-	  $numgoles2016 = $_REQUEST['ngoles2016'];
-      $numgoles2017 = $_REQUEST['ngoles2017'];
-      $observaciones = $_REQUEST['observaciones'];
+<head>
+</head>
 
-      print ("<H1>Inserción de Equipos de Futbol</H1>\n");
+<body>
 
-   // Comprobar que se han introducido todos los datos obligatorios
-      $errores = "";
-	  if (trim($nomequipo) == "")
-         $errores = $errores . "   <LI>El nombre del equipo es obligatorio\n";
-      if (trim($nomciudad) == "")
-         $errores = $errores . "   <LI>El nombre de la ciudad es obligatorio\n"; 
-	  if (trim($nompresidente) == "")
-         $errores = $errores . "   <LI>El nombre del presidente es obligatorio\n";
-      if (trim($aniofundacion) == "")
-         $errores = $errores . "   <LI>El anio de fundacion es obligatorio\n";
-      if (!is_numeric($numgoles2015))
-         $errores = $errores . "   <LI>El numero de goles de 2015 debe ser un valor numérico\n";
-	  if (!is_numeric($numgoles2016))
-         $errores = $errores . "   <LI>El numero de goles de 2016 debe ser un valor numérico\n";
-	  if (!is_numeric($numgoles2017))
-         $errores = $errores . "   <LI>El numero de goles de 2017 debe ser un valor numérico\n";
+<?php
+if (isset($_REQUEST['insertar'])) {
+	$nombre = $_REQUEST['nombre'];
+	$ciudad = $_REQUEST['ciudad'];
+	$presidente = $_REQUEST['presidente'];
+	$fundacion = $_REQUEST['fundacion'];
+	$goles15 = $_REQUEST['goles15'];
+  $goles16 = $_REQUEST['goles16'];
+  $goles17 = $_REQUEST['goles17'];
+  $observaciones = $_REQUEST['observaciones'];
 
-   // Mostrar errores encontrados
-      if ($errores != "")
-      {
-         print ("<P>No se ha podido realizar la inserción debido a los siguientes errores:</P>\n");
-         print ("<UL>");
-         print ($errores);
-         print ("</UL>");
-         print ("<P>[ <A HREF='javascript:history.back()'>Volver</A> ]</P>\n");
-      }
-      else
-      {
-         print ("<P>Estos son los datos introducidos:</P>\n");
-         print ("<UL>\n");
-         print ("   <LI>Nombre del Equipo: $nomequipo\n");
-         print ("   <LI>Nombre de la Ciudad: $nomciudad\n");
-         print ("   <LI>Nombre del Presidente: $nompresidente\n");
-		 print ("   <LI>Anio de Fundacion: $aniofundacion\n");
-		 print ("   <LI>Numero de Goles 2015: $numgoles2015\n");
-		 print ("   <LI>Numero de Goles 2016: $numgoles2016\n");
-		 print ("   <LI>Numero de Goles 2017: $numgoles2017\n");
-		 print ("\n");
-         print ("   <LI>Observaciones: $observaciones\n");
-         print ("</UL>\n");
-         print ("<P>[ <A HREF='Ejercicio3-17-01-18-Futbol.php'>Insertar otro equipo de futbol</A> ]</P>");
-      }
-   }
-   else
-   {
+		$error ="";
+		if (trim($nombre) == "")
+			$error = $error .
+			"<li>El nombre del equipo es obligatorio\n";
+		if (trim($presidente) == "")
+			$error = $error .
+			"<li>El nombre del presidente es obligatorio\n";
+		if(trim($fundacion) == "")
+			$error = $error .
+		    "<li> El ano de fundacion es obligatorio\n";
+	if ($error != "") {
+		echo "<h1>ERROR</h1>\n";
+		echo"<h1>No se ha podido introducir los datos por los siguientes motivos:</h1>\n".
+		"<ul>".
+		$error.
+		"</ul>".
+		"[<A HREF='javascript:history.back()'> Volver </A>]\n";
+}
+    if ($error == ""){
+		echo "Estos son los datos introducidos:\n".
+		"<ul>\n".
+		"<li>Nombre del equipo: $nombre\n".
+		"<li>Ciudad: $ciudad\n".
+		"<li>presidente: $presidente\n".
+		"<li>Ano de fundacion: $fundacion\n".
+		"<li>Numero de goles en 2015: $goles15\n".
+   		 "<li>Numero de goles en 2016: $goles16\n".
+   		 "<li>Numero de goles en 2017: $goles17\n".
+		"<li>Observaciones: $observaciones\n".
+		"</ul>".
+		"[<A HREF='Ejercicio 4.php'> Volver </A>]\n";
+}
+}
+else {
 ?>
+<h1>insercion de equipos de futbol</h1>
 
-<H1>Inserción de Equipos de Futbol</H1>
+<p>introduzca los datos de un equipo:</p>
 
-<P>Introduzca los datos de un equipo de futbol:</P>
+<form method="post">
 
-<FORM CLASS="borde" ACTION="Ejercicio3-17-01-18-Futbol.php" METHOD="POST">
+<p><label>nombre del equipo:</label>
+<input type="text" name="nombre"></p>
 
-<P><LABEL>Nombre del Equipo:</LABEL>
-<INPUT TYPE="TEXT" NAME="nequipo"></P>
+<p><label>ciudad: </label>
+<select name="ciudad">
+   <option value="madrid" selected>Madrid
+   <option value="barcelona">Barcelona
+   <option value="sevilla">Sevilla
+   <option value="valencia">Valencia
+</select></p>
 
-<P><LABEL>Ciudad:</LABEL>
-<SELECT NAME="nciudad">
-   <OPTION VALUE="Madrid" SELECTED>Madrid
-   <OPTION VALUE="Barcelona">Barcelona
-   <OPTION VALUE="Sevilla">Sevilla
-   <OPTION VALUE="Valencia">Valencia
-</SELECT></P>
+<p><label>nombre del presidente</label>
+<input type="text" name="presidente"></p>
 
-<P><LABEL>Nombre del Presidente:</LABEL>
-<INPUT TYPE="TEXT" NAME="npresidente"></P>
+<p><label>ano de fundacion: </label>
+<input type="text" name="fundacion"></p>
 
-<P><LABEL>Anio de Fundacion:</LABEL>
-<INPUT TYPE="TEXT" NAME="naniofundacion"></P>
+<p><label>numero de goles 2015</label>
+<input type="radio" name="goles15" value="30">30
+<input type="radio" name="goles15" value="40">40
+<input type="radio" name="goles15" value="50" checked>50
+<input type="radio" name="goles15" value="60">60
+<input type="radio" name="goles15" value="70">70</p>
 
+<p><label>numero de goles 2016</label>
+<input type="radio" name="goles16" value="30">30
+<input type="radio" name="goles16" value="40">40
+<input type="radio" name="goles16" value="50" checked>50
+<input type="radio" name="goles16" value="60">60
+<input type="radio" name="goles16" value="70">70</p>
 
-<P><LABEL>Número de goles 2015:</LABEL>
-<INPUT TYPE="radio" NAME="ngoles2015" VALUE="30">30
-<INPUT TYPE="radio" NAME="ngoles2015" VALUE="40">40
-<INPUT TYPE="radio" NAME="ngoles2015" VALUE="50" CHECKED>50
-<INPUT TYPE="radio" NAME="ngoles2015" VALUE="60">60
-<INPUT TYPE="radio" NAME="ngoles2015" VALUE="70">70</P>
+<p><label>numero de goles 2017</label>
+<input type="radio" name="goles17" value="30">30
+<input type="radio" name="goles17" value="40">40
+<input type="radio" name="goles17" value="50" checked>50
+<input type="radio" name="goles17" value="60">60
+<input type="radio" name="goles17" value="70">70</p>
 
-<P><LABEL>Número de goles 2016:</LABEL>
-<INPUT TYPE="radio" NAME="ngoles2016" VALUE="30">30
-<INPUT TYPE="radio" NAME="ngoles2016" VALUE="40">40
-<INPUT TYPE="radio" NAME="ngoles2016" VALUE="50" CHECKED>50
-<INPUT TYPE="radio" NAME="ngoles2016" VALUE="60">60
-<INPUT TYPE="radio" NAME="ngoles2016" VALUE="70">70</P>
+<p><label>observaciones:</label>
+<textarea name="observaciones" cols="50" rows="5"></textarea></p>
 
-<P><LABEL>Número de goles 2017:</LABEL>
-<INPUT TYPE="radio" NAME="ngoles2017" VALUE="30">30
-<INPUT TYPE="radio" NAME="ngoles2017" VALUE="40">40
-<INPUT TYPE="radio" NAME="ngoles2017" VALUE="50" CHECKED>50
-<INPUT TYPE="radio" NAME="ngoles2017" VALUE="60">60
-<INPUT TYPE="radio" NAME="ngoles2017" VALUE="70">70</P>
+<p><input type="submit" name="insertar" value="insertar equipo"></p>
 
-<P><LABEL>Observaciones:</LABEL>
-<TEXTAREA NAME="observaciones" COLS="50" ROWS="5"></TEXTAREA></P>
-
-<P><INPUT TYPE="submit" NAME="insertar" VALUE="Insertar Equipo"></P>
-
-</FORM>
-
-<?PHP
-   }
-?>
-
-</BODY>
-</HTML>
+</form>
+<?php
+}
+ ?>
+</body>
+</html>
